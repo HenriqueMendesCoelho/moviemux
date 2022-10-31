dd882d6ed5e6b49116b6e3d9cccbb87750f65519
KronusBoss <KronusBoss>
2022-10-31 12:00:24 -0300

    Fix customAlert color buttons

--- src/domain/alerts/CustomAlerts.ts
+++ src/domain/alerts/CustomAlerts.ts
@@ -1,4 +1,4 @@
-import Swal from "sweetalert2";
+import Swal from 'sweetalert2';
 
 export default class CustomAlerts {
   // eslint-disable-next-line
@@ -6,16 +6,17 @@ export default class CustomAlerts {
     return Swal.fire({
       title: title,
       text: text,
-      icon: confirm ? "question" : icon,
+      icon: confirm ? 'question' : icon,
       allowOutsideClick: false,
       showConfirmButton: true,
       showCancelButton: confirm ? true : false,
-      background: "#293244",
-      confirmButtonText: confirm ? "Sim" : "OK",
-      cancelButtonText: "Cancelar",
-      confirmButtonColor: "#00f6ff",
+      background: '#293244',
+      confirmButtonText: confirm ? 'Sim' : 'OK',
+      cancelButtonText: 'Cancelar',
+      confirmButtonColor: '#21ba45',
+      cancelButtonColor: '#f44336',
       allowEscapeKey: false,
-      color: "#f0ffffde",
+      color: '#f0ffffde',
     });
   }
 
@@ -23,15 +24,15 @@ export default class CustomAlerts {
   toastAlert(title: string, icon: any) {
     const toast = Swal.mixin({
       toast: true,
-      position: "top-end",
+      position: 'top-end',
       showConfirmButton: false,
-      background: "#293244",
-      color: "#f0ffffde",
+      background: '#293244',
+      color: '#f0ffffde',
       timer: 3000,
       timerProgressBar: true,
       didOpen: (toast) => {
-        toast.addEventListener("mouseenter", Swal.stopTimer);
-        toast.addEventListener("mouseleave", Swal.resumeTimer);
+        toast.addEventListener('mouseenter', Swal.stopTimer);
+        toast.addEventListener('mouseleave', Swal.resumeTimer);
       },
     });
 
@@ -44,13 +45,15 @@ export default class CustomAlerts {
   inputConfirm(title: string, label: string, placeHolder: string) {
     return Swal.fire({
       title: title,
-      input: "password",
+      input: 'password',
       inputLabel: label,
       inputPlaceholder: placeHolder,
-      cancelButtonText: "Cancelar",
-      background: "#293244",
-      color: "#f0ffffde",
+      cancelButtonText: 'Cancelar',
+      background: '#293244',
+      color: '#f0ffffde',
       showCancelButton: true,
+      confirmButtonColor: '#21ba45',
+      cancelButtonColor: '#f44336',
     });
   }
 }
