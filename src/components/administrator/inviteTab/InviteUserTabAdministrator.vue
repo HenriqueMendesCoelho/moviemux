27c72006cb7a399fc5917738ce8468a901be10b2
KronusBoss <KronusBoss>
2022-11-20 02:29:51 -0300

    Initial refactoring AdministratorPage

---
+++ src/components/administrator/inviteTab/InviteUserTabAdministrator.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <h4>Invite Tab</h4>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'ListUserTabAdministrator',
+});
+</script>
+
+<style></style>
---
+++ src/components/administrator/userTab/UserTabAdministrator.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <h4>User Tab</h4>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'UserTabAdministrator',
+});
+</script>
+
+<style></style>
