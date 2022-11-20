30b1b49000b66c80801f90852cde21f8e02d0d97
KronusBoss <KronusBoss>
2022-11-20 04:14:36 -0300

    update

---
+++ src/components/notFound/NotFound.vue
@@ -0,0 +1,62 @@
+<template>
+  <main class="flex flex-center">
+    <div class="row justify-center">
+      <div>
+        <router-link to="/">
+          <img
+            src="../../assets/logo-kronus.png"
+            class="left-img-login"
+            alt="logo"
+            draggable="false"
+            @click="(forgotPass = false), (createAccout = false)"
+          />
+        </router-link>
+      </div>
+      <div class="col-12"></div>
+      <p>404,</p>
+      <div class="col-12"></div>
+      <h1>página não encontrada...</h1>
+    </div>
+  </main>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'NotFound',
+  setup() {
+    document.title = 'Cineminha - 404';
+  },
+});
+</script>
+
+<style lang="scss">
+main {
+  height: 100vh;
+  h1 {
+    color: white;
+  }
+
+  p {
+    font-size: 35vh;
+    color: white;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .left-img-login {
+    width: 12vw;
+    border-radius: 50%;
+
+    cursor: pointer;
+
+    &:hover {
+      box-shadow: 0px 0px 30px -12px var(--primary);
+
+      transition: 0.2s ease-in-out;
+    }
+  }
+}
+</style>
