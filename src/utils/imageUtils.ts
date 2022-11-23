50992963ee291f84c84bfd024b65cb0ecd463a95
KronusBoss <KronusBoss>
2022-11-23 15:57:25 -0300

    Adjust validation image for accept only images

--- src/utils/imageUtils.ts
+++ src/utils/imageUtils.ts
@@ -2,17 +2,16 @@ export default {
   async checkImageSizeByUrl(url: string) {
     const image = new Image();
     return new Promise<{ imgOk: boolean; error: boolean }>(function (resolve, reject) {
+      if (/\.(jpg|jpeg|png|webp|)$/.test(url)) {
+        reject({ error: true });
+      }
+
       image.addEventListener('load', function () {
         if (!this.naturalHeight || !this.naturalWidth) {
           resolve({ imgOk: false, error: false });
         }
 
-        if (
-          this.naturalHeight < 350 ||
-          this.naturalHeight > 1200 ||
-          this.naturalWidth > 1100 ||
-          this.naturalWidth < 550
-        ) {
+        if (this.naturalHeight < 350 || this.naturalHeight > 1200 || this.naturalWidth > 1100 || this.naturalWidth < 550) {
           resolve({ imgOk: false, error: false });
         }
 
