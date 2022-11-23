449f7b6e0f43f05086d824b1412abea42e09ef20
KronusBoss <KronusBoss>
2022-11-23 15:58:08 -0300

    Refactor and rename TableStylized to TableEditDelete

---
+++ src/components/shared/tables/TableEditDelete.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-table
+    :title="title"
+    :rows="rows"
+    :style="`background-color: ${color};`"
+    dense
+    dark
+    card-class="text-white"
+    row-key="index"
+    virtual-scroll
+    no-data-label="Não encontrei nada :("
+    :rows-per-page-options="[0]"
+    :columns="columns"
+  >
+    <template v-slot:body-cell-actions="props">
+      <q-td :props="props">
+        <q-btn dense round flat color="white" icon="edit" v-if="edit">
+          <q-tooltip>Editar</q-tooltip>
+        </q-btn>
+        <q-btn dense round flat color="white" icon="delete">
+          <q-tooltip>Deletar</q-tooltip>
+        </q-btn>
+      </q-td>
+    </template>
+  </q-table>
+</template>
+
+<script lang="ts">
+import type { QTableProps } from 'quasar';
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  name: 'TableEditDelete',
+  props: {
+    title: {
+      type: String,
+    },
+    color: {
+      type: String,
+      default: '#343c4c',
+    },
+    columns: {
+      type: Array as PropType<QTableProps['columns']>,
+      required: true,
+    },
+    rows: {
+      type: Array,
+      required: false,
+    },
+    edit: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+  },
+});
+</script>
