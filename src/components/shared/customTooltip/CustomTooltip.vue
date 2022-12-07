7547403f30635f0a67ba17e334c07b139cee6ff3
KronusBoss <henriquemendescoelho@gmail.com>
2022-12-07 03:29:37 -0300

    feat(CustomTooltip): new component tooltip

---
+++ src/components/shared/customTooltip/CustomTooltip.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-tooltip style="font-size: 10pt; background-color: rgb(41, 50, 68, 0.9)"><slot></slot></q-tooltip>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CustomTooltip',
+});
+</script>
