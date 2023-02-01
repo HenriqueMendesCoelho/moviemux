569e661c966a5b8da46834d81297ccee416bdd9c
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-02-01 02:24:10 -0300

    chore: removing sweetalert

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,9 @@ import { routes } from './routes/routes';
 
 import { createPinia } from 'pinia';
 
-import VueSweetalert2 from 'vue-sweetalert2';
 import { Quasar } from 'quasar';
 import quasarUserOptions from './quasar-user-options';
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
-
-import 'sweetalert2/dist/sweetalert2.min.css';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -25,6 +22,5 @@ app.use(createPinia());
 
 app.use(Quasar, quasarUserOptions);
 app.use(autoAnimatePlugin);
-app.use(VueSweetalert2);
 
 app.mount('#app');
