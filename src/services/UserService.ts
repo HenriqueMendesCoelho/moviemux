11c103d6aa43010f3e4dece7f6dc4465ca7e8da3
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-03-21 12:35:08 -0300

    fix: UserService to return data from request

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,13 +1,31 @@
 import axios from 'axios';
 
+import User from '@/domain/user/User';
+
 const BASE_URL = process.env.VUE_APP_KB_CINE_API;
 const API_USER = `${BASE_URL}/api/user`;
 
 export default {
   async create(payload: { name: string; email: string; password: string; invite_code: string }) {
     try {
-      await axios.post(`${API_USER}`, payload);
-      return Promise.resolve();
+      const res = await axios.post(`${API_USER}`, payload);
+      return res.data;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  },
+  async getUserAdm(email: string): Promise<User> {
+    try {
+      const res = await axios.get(`${API_USER}/adm?email=${email}`);
+      return res.data;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  },
+  async getUser(): Promise<User> {
+    try {
+      const res = await axios.get(`${API_USER}`);
+      return res.data;
     } catch (error) {
       return Promise.reject(error);
     }
