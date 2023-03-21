a1d2c47bbdad04357e1b515355aeabecdb5a3758
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-03-21 12:32:18 -0300

    refactor: Created user domain to PanelUserInfo

--- src/components/shared/panelUserInfo/PanelUserInfo.vue
+++ src/components/shared/panelUserInfo/PanelUserInfo.vue
@@ -6,8 +6,8 @@
       </div>
       <div :class="isMobile ? 'col-12' : 'col'">
         <q-toggle
+          v-model="user.preferences.notify"
           size="xl"
-          v-model="user.notifications"
           color="kb-primary"
           checked-icon="notifications"
           unchecked-icon="notifications_off"
@@ -16,35 +16,45 @@
         <CustomTooltip anchor="bottom middle" :delay="500" style="font-size: 10pt">Notificações por e-mail</CustomTooltip>
       </div>
     </div>
-    <div class="q-mt-md" :class="isMobile ? 'col-12' : 'col-8'">
-      <InputText :label="'Email'" v-model="user.email" :readOnly="true" />
+    <div class="q-mt-md" :class="isMobile ? 'col-12 row' : 'col-8 row'">
+      <InputText class="col" :label="'Email'" v-model="user.email" :readOnly="allReadOnly" />
+      <q-input
+        v-model="user.statistics.consecutive_failed_login_attempts"
+        v-if="showAdmInfo"
+        :class="isMobile ? 'col-12' : 'col-6 q-ml-sm q-pl-sm'"
+        square
+        filled
+        standout="text-info"
+        color="info"
+        label="Quantidade de tentativas de login"
+        dark
+        :readonly="true"
+      />
     </div>
     <div class="q-mt-md" :class="isMobile ? 'col-12' : 'col-8'">
       <div class="row">
         <div :class="isMobile ? 'col-12' : 'col-6 q-pr-sm'">
           <q-input
+            :model-value="user.roles.join(',')"
             square
             filled
             standout="text-info"
             color="info"
-            v-model="user.accessProfile"
             label="Perfil de Acesso"
-            style="background-color: #343c4c"
             dark
-            :readonly="allReadOnly"
+            :readonly="true"
           />
         </div>
         <div :class="isMobile ? 'col-12' : 'col-6 q-pl-sm'">
           <q-input
+            :model-value="new Date(user.created_at).toLocaleString()"
             square
             filled
             standout="text-info"
             color="info"
-            :model-value="user.dtCreated.toLocaleString('pt-br')"
             label="Data criação da conta"
-            style="background-color: #343c4c"
             dark
-            :readonly="allReadOnly"
+            :readonly="true"
           />
         </div>
       </div>
@@ -53,27 +63,26 @@
       <div class="row">
         <div :class="isMobile ? 'col-12' : 'col-6 q-pr-sm'">
           <q-input
+            :model-value="new Date(user.updated_at).toLocaleString()"
             square
             filled
             standout="text-info"
             color="info"
-            v-model="user.qtdMovies"
             label="Quantidade de filmes cadastrados"
             dark
-            :readonly="allReadOnly"
+            :readonly="true"
           />
         </div>
         <div :class="isMobile ? 'col-12 q-mt-md' : 'col-6 q-pl-sm'">
           <q-input
+            :model-value="user.statistics.ratings_given"
             square
             filled
             standout="text-info"
             color="info"
-            v-model="user.qtdNotes"
             label="Quantidade de notas cadastrados"
             dark
-            :readonly="allReadOnly"
-            draggable="false"
+            :readonly="true"
           />
         </div>
       </div>
@@ -84,27 +93,23 @@
 <script lang="ts" scoped>
 import { defineComponent, PropType } from 'vue';
 
+import User from '@/domain/user/User';
+
 import InputText from '@/components/shared/inputText/InputText.vue';
 import CustomTooltip from '@/components/shared/customTooltip/CustomTooltip.vue';
-
-type userProp = {
-  name: string;
-  email: string;
-  accessProfile: string;
-  dtCreated: Date;
-  qtdMovies: number;
-  qtdNotes: number;
-  notifications: boolean;
-};
 
 export default defineComponent({
   name: 'PanelUserInfo',
   props: {
     modelValue: {
-      type: Object as PropType<userProp>,
-      required: false,
+      type: Object as PropType<User>,
+      required: true,
     },
     allReadOnly: {
+      type: Boolean,
+      default: false,
+    },
+    showAdmInfo: {
       type: Boolean,
       default: false,
     },
@@ -112,41 +117,26 @@ export default defineComponent({
   components: { InputText, CustomTooltip },
   data() {
     return {
-      user: {
-        name: '',
-        email: '',
-        accessProfile: '',
-        dtCreated: new Date(),
-        qtdMovies: 0,
-        qtdNotes: 0,
-        notifications: false,
-      } as userProp,
+      no: true,
+      user: new User(),
     };
   },
   emits: ['update:modelValue'],
   mounted() {
-    this.updateForm();
+    if (this.modelValue) {
+      this.user = this.modelValue;
+    }
   },
   computed: {
     isMobile(): boolean | undefined {
       return this.$q.platform.is.mobile;
     },
   },
-  methods: {
-    updateForm() {
-      this.user.name = this.modelValue?.name ?? '';
-      this.user.email = this.modelValue?.email ?? '';
-      this.user.accessProfile = this.modelValue?.accessProfile ?? '';
-      this.user.dtCreated = this.modelValue?.dtCreated ?? new Date();
-      this.user.qtdMovies = this.modelValue?.qtdMovies ?? 0;
-      this.user.qtdNotes = this.modelValue?.qtdNotes ?? 0;
-      this.user.notifications = this.modelValue?.notifications ?? false;
-    },
-  },
   watch: {
     modelValue() {
-      this.updateForm();
-      if (this.modelValue) this.user = this.modelValue;
+      if (this.modelValue) {
+        this.user = this.modelValue;
+      }
     },
     user() {
       this.$emit('update:modelValue', this.user);
