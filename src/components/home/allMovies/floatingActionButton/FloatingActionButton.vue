b6341d8080db6978765edfa81504bdb76ba793da
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-04-16 12:53:41 -0300

    refactor: FloatingActionButton home

--- src/components/home/allMovies/floatingActionButton/FloatingActionButton.vue
+++ src/components/home/allMovies/floatingActionButton/FloatingActionButton.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="absolute-bottom-right" style="position: fixed; bottom: 5vh; right: 5vh">
     <q-fab color="kb-primary" icon="keyboard_arrow_up" direction="up">
-      <q-fab-action color="grey-mid2" @click="pushToMovie" icon="add"
-        ><CustomTooltip anchor="bottom middle" :offset="[0, 10]" :delay="800">ADICIONAR FILME</CustomTooltip></q-fab-action
-      >
-      <q-fab-action color="grey-mid2" @click="scrollToTop" icon="keyboard_arrow_up"
-        ><CustomTooltip anchor="bottom middle" :offset="[0, 10]" :delay="800">IR PARA O TOPO</CustomTooltip></q-fab-action
+      <q-fab-action
+        color="grey-mid2"
+        :to="{ name: 'add' }"
+        icon="add"
+        :external-label="true"
+        label="ADICIONAR FILME"
+        label-class="bg-grey-mid2"
+        label-position="left"
+      />
+      <q-fab-action
+        color="grey-mid2"
+        @click="scrollToTop"
+        icon="keyboard_arrow_up"
+        :external-label="true"
+        label="IR PARA O TOPO"
+        label-class="bg-grey-mid2"
+        label-position="left"
+      />
       >
     </q-fab>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { useRouter } from 'vue-router';
-
-import CustomTooltip from '@/components/shared/customTooltip/CustomTooltip.vue';
-
-const router = useRouter();
-
-function pushToMovie(): void {
-  router.push({ name: 'add' });
-}
 function scrollToTop(): void {
   window.scrollTo({
     top: 0,
