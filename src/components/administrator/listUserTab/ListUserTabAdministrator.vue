c558ca4b15039e17247cc236d416acfd121ac7a9
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-04-18 11:59:07 -0300

    refactor: ListUserTabAdministrator to composition api

--- src/components/administrator/listUserTab/ListUserTabAdministrator.vue
+++ src/components/administrator/listUserTab/ListUserTabAdministrator.vue
@@ -16,124 +16,112 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
 import { useQuasar } from 'quasar';
 import type { QTableProps } from 'quasar';
 
-import TableCopyDelete from '@/components/shared/tableCopyDelete/TableCopyDelete.vue';
-import SeparatorDivLineSolid from '@/components/shared/separator/SeparatorDivLineSolid.vue';
 import User from '@/domain/user/User';
 import UserService from '@/services/UserService';
 
-export default defineComponent({
-  name: 'ListUserTabAdministrator',
-  components: {
-    TableCopyDelete,
-    SeparatorDivLineSolid,
+import TableCopyDelete from '@/components/shared/tableCopyDelete/TableCopyDelete.vue';
+import SeparatorDivLineSolid from '@/components/shared/separator/SeparatorDivLineSolid.vue';
+
+const $q = useQuasar();
+const columns: QTableProps['columns'] = [
+  {
+    name: 'nome',
+    label: 'Nome',
+    field: 'name',
+    align: 'left',
   },
-  setup() {
-    const $q = useQuasar();
-    const columns: QTableProps['columns'] = [
-      {
-        name: 'nome',
-        label: 'Nome',
-        field: 'name',
-        align: 'left',
-      },
-      {
-        name: 'email',
-        label: 'E-mail',
-        field: 'email',
-        align: 'center',
-      },
-      {
-        name: 'roles',
-        label: 'Perfis de acesso',
-        field: 'roles',
-        align: 'center',
-        format: (val) => val.join(', '),
-      },
-      {
-        name: 'dtCreated',
-        label: 'Data de criação',
-        field: 'created_at',
-        align: 'center',
-        format: (val) => new Date(val).toLocaleString(),
-      },
-      {
-        name: 'dtUpdated',
-        label: 'Data da ultima modificação',
-        field: 'updated_at',
-        align: 'center',
-        format: (val) => new Date(val).toLocaleString(),
-      },
-      {
-        name: 'actions',
-        label: '',
-        field: '',
-        align: 'center',
-      },
-    ];
-    return {
-      columns,
-      showSuccess(msg: string) {
-        $q.notify({
-          type: 'positive',
-          message: msg,
-          position: 'top',
-        });
-      },
-      showError(msg: string) {
-        $q.notify({
-          type: 'negative',
-          message: msg,
-          position: 'top',
-        });
-      },
-    };
+  {
+    name: 'email',
+    label: 'E-mail',
+    field: 'email',
+    align: 'center',
   },
-  data() {
-    return {
-      users: [] as User[],
-      loading: false,
-    };
+  {
+    name: 'roles',
+    label: 'Perfis de acesso',
+    field: 'roles',
+    align: 'center',
+    format: (val) => val.join(', '),
   },
-  async mounted() {
-    try {
-      const res = await UserService.listUsers();
-      this.users = res;
-    } catch {
-      this.showError('Erro ao buscar usuarios');
-    }
+  {
+    name: 'dtCreated',
+    label: 'Data de criação',
+    field: 'created_at',
+    align: 'center',
+    format: (val) => new Date(val).toLocaleString(),
   },
-  methods: {
-    actionCopy(user?: User) {
-      if (!user) {
-        this.showError('Nenhum usuario encontrado');
-        return;
-      }
-      if (!user.email) {
-        this.showError('Usuario não possui e-mail');
-        return;
-      }
-      navigator.clipboard.writeText(user.email);
-      this.showSuccess('E-mail do usuario copiado');
-    },
-    async actionDelete(user?: User) {
-      if (!user) {
-        return;
-      }
-      try {
-        this.loading = true;
-        await UserService.deleteUser(user.id);
-        this.showSuccess('Usuario deletado');
-      } catch {
-        this.showError('Erro ao deletar usuario');
-      } finally {
-        this.loading = false;
-      }
-    },
+  {
+    name: 'dtUpdated',
+    label: 'Data da ultima modificação',
+    field: 'updated_at',
+    align: 'center',
+    format: (val) => new Date(val).toLocaleString(),
   },
+  {
+    name: 'actions',
+    label: '',
+    field: '',
+    align: 'center',
+  },
+];
+const users = ref<User[]>([]);
+const loading = ref(false);
+
+function showSuccess(msg: string) {
+  $q.notify({
+    type: 'positive',
+    message: msg,
+    position: 'top',
+  });
+}
+
+function showError(msg: string) {
+  $q.notify({
+    type: 'negative',
+    message: msg,
+    position: 'top',
+  });
+}
+
+onMounted(async () => {
+  try {
+    const res = await UserService.listUsers();
+    users.value = res;
+  } catch {
+    showError('Erro ao buscar usuarios');
+  }
 });
+
+function actionCopy(user?: User) {
+  if (!user) {
+    showError('Nenhum usuario encontrado');
+    return;
+  }
+  if (!user.email) {
+    showError('Usuario não possui e-mail');
+    return;
+  }
+  navigator.clipboard.writeText(user.email);
+  showSuccess('E-mail do usuario copiado');
+}
+
+async function actionDelete(user?: User) {
+  if (!user) {
+    return;
+  }
+  try {
+    loading.value = true;
+    await UserService.deleteUser(user.id);
+    showSuccess('Usuario deletado');
+  } catch {
+    showError('Erro ao deletar usuario');
+  } finally {
+    loading.value = false;
+  }
+}
 </script>
