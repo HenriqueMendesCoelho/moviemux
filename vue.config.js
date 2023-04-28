118485d3d23096fb4fa6631055358cacbd2da024
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-04-28 12:41:41 -0300

    refactor: remove cache from client for always be in the last version

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: ['quasar'],
-
+  filenameHashing: true,
   devServer: { port: 3000 },
-
   pluginOptions: {
     quasar: {
       importStrategy: 'kebab',
