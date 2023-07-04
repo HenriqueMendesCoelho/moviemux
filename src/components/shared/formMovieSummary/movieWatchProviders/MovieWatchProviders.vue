6b048eabe393ace82028a37eb91bca8b0ddcb65c
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-07-04 11:59:51 -0300

    refactor: MovieWatchProviders loading spinner

--- src/components/shared/formMovieSummary/movieWatchProviders/MovieWatchProviders.vue
+++ src/components/shared/formMovieSummary/movieWatchProviders/MovieWatchProviders.vue
@@ -1,44 +1,52 @@
 <template>
-  <div class="row no-wrap scroll q-gutter-lg">
-    <div class="col-auto" v-if="watchProviders?.results['BR']?.flatrate?.length">
-      <div class="text-h6">Stream</div>
-      <div class="row no-wrap q-py-xs q-gutter-sm">
-        <div v-for="provider in watchProviders?.results['BR']?.flatrate" :key="provider.provider_id">
-          <q-avatar><img :src="getURL(provider.logo_path)" /></q-avatar>
-          <CustomTooltip delay="400">{{ provider.provider_name }}</CustomTooltip>
+  <div>
+    <q-spinner color="kb-primary" size="xl" v-if="!watchProviders?.results" />
+    <div class="row no-wrap scroll q-gutter-lg">
+      <div class="col-auto" v-if="watchProviders?.results['BR']?.flatrate?.length">
+        <div class="text-h6">Stream</div>
+        <div class="row no-wrap q-py-xs q-gutter-sm">
+          <div v-for="provider in watchProviders?.results['BR']?.flatrate" :key="provider.provider_id">
+            <q-avatar><img :src="getURL(provider.logo_path)" /></q-avatar>
+            <CustomTooltip delay="400">{{ provider.provider_name }}</CustomTooltip>
+          </div>
         </div>
       </div>
-    </div>
-    <q-separator dark vertical inset v-if="watchProviders?.results['BR']?.rent?.length && watchProviders?.results['BR'].flatrate?.length" />
-    <div class="col-auto" v-if="watchProviders?.results['BR']?.rent?.length">
-      <div class="text-h6">Alugar</div>
-      <div class="row no-wrap q-py-xs q-gutter-sm">
-        <div v-for="provider in watchProviders?.results['BR']?.rent" :key="provider.provider_id">
-          <q-avatar><img :src="getURL(provider.logo_path)" /></q-avatar>
-          <CustomTooltip delay="400">{{ provider.provider_name }}</CustomTooltip>
+      <q-separator
+        dark
+        vertical
+        inset
+        v-if="watchProviders?.results['BR']?.rent?.length && watchProviders?.results['BR'].flatrate?.length"
+      />
+      <div class="col-auto" v-if="watchProviders?.results['BR']?.rent?.length">
+        <div class="text-h6">Alugar</div>
+        <div class="row no-wrap q-py-xs q-gutter-sm">
+          <div v-for="provider in watchProviders?.results['BR']?.rent" :key="provider.provider_id">
+            <q-avatar><img :src="getURL(provider.logo_path)" /></q-avatar>
+            <CustomTooltip delay="400">{{ provider.provider_name }}</CustomTooltip>
+          </div>
         </div>
       </div>
-    </div>
-    <q-separator dark vertical inset v-if="watchProviders?.results['BR']?.buy?.length && watchProviders?.results['BR'].rent?.length" />
-    <div class="col-auto" v-if="watchProviders?.results['BR']?.buy?.length">
-      <div class="text-h6">Comprar</div>
-      <div class="row no-wrap q-py-xs q-gutter-sm">
-        <div v-for="provider in watchProviders?.results['BR'].buy" :key="provider.provider_id">
-          <q-avatar><img :src="getURL(provider.logo_path)" /></q-avatar>
-          <CustomTooltip delay="400">{{ provider.provider_name }}</CustomTooltip>
+      <q-separator dark vertical inset v-if="watchProviders?.results['BR']?.buy?.length && watchProviders?.results['BR'].rent?.length" />
+      <div class="col-auto" v-if="watchProviders?.results['BR']?.buy?.length">
+        <div class="text-h6">Comprar</div>
+        <div class="row no-wrap q-py-xs q-gutter-sm">
+          <div v-for="provider in watchProviders?.results['BR'].buy" :key="provider.provider_id">
+            <q-avatar><img :src="getURL(provider.logo_path)" /></q-avatar>
+            <CustomTooltip delay="400">{{ provider.provider_name }}</CustomTooltip>
+          </div>
         </div>
       </div>
-    </div>
-    <div
-      v-if="
-        !watchProviders?.results['BR']?.buy?.length &&
-        !watchProviders?.results['BR']?.flatrate?.length &&
-        !watchProviders?.results['BR']?.rent?.length &&
-        watchProviders?.results
-      "
-    >
-      <q-icon name="cloud_off" size="md" color="white" />
-      <CustomTooltip delay="400">Sem informações das plataformas dísponiveis no Brasil</CustomTooltip>
+      <div
+        v-if="
+          !watchProviders?.results['BR']?.buy?.length &&
+          !watchProviders?.results['BR']?.flatrate?.length &&
+          !watchProviders?.results['BR']?.rent?.length &&
+          watchProviders?.results
+        "
+      >
+        <q-icon name="cloud_off" size="md" color="white" />
+        <CustomTooltip delay="400">Sem informações das plataformas dísponiveis no Brasil</CustomTooltip>
+      </div>
     </div>
   </div>
 </template>
