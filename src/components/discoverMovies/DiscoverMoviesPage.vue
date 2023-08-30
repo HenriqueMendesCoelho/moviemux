1eb4ce323f2d520d90ec9ccbe5794ef9bda8ce15
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-08-30 08:12:46 -0300

    refactor: DiscoverMovie on dialog open update url

--- src/components/discoverMovies/DiscoverMoviesPage.vue
+++ src/components/discoverMovies/DiscoverMoviesPage.vue
@@ -33,13 +33,14 @@
         <q-infinite-scroll ref="infinitScrollRef" class="full-width" @load="onLoad" :offset="1500">
           <div class="row justify-center q-col-gutter-xl">
             <div class="col-auto" v-for="(movie, index) in movies" :key="index">
-              <CardImageDiscoverMovies
-                v-model="wishlists"
-                @click-on-image="showDialog(movie.id)"
-                :movie="movie"
-                @call-tmdb="cardCallTmdb($event, movie.id)"
-                @copy-url="copyMovie($event)"
-              />
+              <router-link :to="`/movie/discover?movie=${movie.id}`" class="button">
+                <CardImageDiscoverMovies
+                  v-model="wishlists"
+                  :movie="movie"
+                  @call-tmdb="cardCallTmdb($event, movie.id)"
+                  @copy-url="copyMovie($event)"
+                />
+              </router-link>
             </div>
           </div>
         </q-infinite-scroll>
@@ -48,7 +49,7 @@
         </div>
         <FloatingActionBtnTop />
       </div>
-      <DialogFormMovieSummary v-model="showDialogMovieSummary" :movie-id="movieIdDialog" position="standard" />
+      <DialogFormMovieSummary v-model="showDialogMovieSummary" :movie-id="movieIdDialog" position="standard" @hide="onHideDialog()" />
     </div>
   </ContainerMain>
 </template>
@@ -57,6 +58,7 @@
 import { computed, onActivated, onMounted, onUpdated, ref, watch } from 'vue';
 import { useQuasar } from 'quasar';
 import { useRoute } from 'vue-router';
+import { useRouter } from 'vue-router';
 
 import { useStyleStore } from 'src/stores/StyleStore';
 
@@ -76,6 +78,7 @@ import KitService from 'src/services/KitService';
 
 const $q = useQuasar();
 const route = useRoute();
+const router = useRouter();
 
 const searchText = ref('');
 const filterOptions = ref([
@@ -121,22 +124,13 @@ function showError(msg: string) {
   });
 }
 
-onMounted(() => {
-  const movieParam = parseInt(route.query.movie?.toString() || '');
-
-  if (!movieParam) {
-    return;
-  }
-
-  showDialog(movieParam);
-});
-
 onMounted(async () => {
   page.value = 1;
   lastPage.value = 2;
   const res = await getMoviesPopular();
   movies.value = res;
   await listWishlist();
+  showDialogByParam();
 });
 
 onActivated(async () => {
@@ -145,6 +139,7 @@ onActivated(async () => {
 
 onUpdated(async () => {
   await listWishlist();
+  showDialogByParam();
 });
 
 watch(
@@ -154,6 +149,18 @@ watch(
   }
 );
 
+function onHideDialog() {
+  router.push('/movie/discover');
+}
+function showDialogByParam() {
+  const movieParam = parseInt(route.query.movie?.toString() || '');
+
+  if (!movieParam) {
+    return;
+  }
+
+  showDialog(movieParam);
+}
 async function cardCallTmdb(typeSearch: { label: string; value: string }, movieId: number) {
   movieIdSelected.value = movieId;
   selectOrder.value = typeSearch;
