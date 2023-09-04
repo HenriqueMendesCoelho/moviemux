002855c91ae513924c55db54d7b1b61a8bc153c3
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-09-04 14:49:04 -0300

    feat: tooltip for show info of wishlist

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -3,4 +3,10 @@ export default {
   getStringParams(params: any) {
     return new URLSearchParams(params).toString();
   },
+  dateToLocaleString(date: string | Date) {
+    return new Date(date).toLocaleString('pt-br', {
+      dateStyle: 'short',
+      timeStyle: 'short',
+    });
+  },
 };
