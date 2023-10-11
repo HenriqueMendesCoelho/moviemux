69b883871f80d3ef38d2b07e920afd76cd473be3
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-10-11 11:21:24 -0300

    fix: type ConfirmDialog

--- src/components/shared/confirmDialog/types/ConfirmDialogType.ts
+++ src/components/shared/confirmDialog/types/ConfirmDialogType.ts
@@ -1,3 +1,3 @@
 export type ConfirmDialogRefType = {
-  dialog: (message: string, title?: string, ok?: string, cancel?: string, persisstent?: boolean) => void;
+  dialog: (message: string, focus?: 'ok' | 'cancel' | 'none', title?: string, ok?: string, cancel?: string, persisstent?: boolean) => void;
 };
