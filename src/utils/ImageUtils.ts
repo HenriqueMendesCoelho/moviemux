e0f1b5497093c225119f7612f4e5e7d1dfd13f38
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-10-18 17:27:18 -0300

    fix adding utils Capitalized

---
+++ src/utils/ImageUtils.ts
@@ -0,0 +1,33 @@
+export default {
+  async checkImageSizeByUrl(url: string, minHeight = 650, maxHeight = 800, minWidth = 500, maxWidth = 500) {
+    const image = new Image();
+    return new Promise<{ imgOk: boolean; error: boolean }>(function (resolve, reject) {
+      if (!/\.(jpg|jpeg|png|webp|)$/.test(url)) {
+        reject({ error: true });
+      }
+
+      image.addEventListener('load', function () {
+        if (!this.naturalHeight || !this.naturalWidth) {
+          resolve({ imgOk: false, error: false });
+        }
+
+        if (
+          this.naturalHeight < minHeight ||
+          this.naturalHeight > maxHeight ||
+          this.naturalWidth > maxWidth ||
+          this.naturalWidth < minWidth
+        ) {
+          resolve({ imgOk: false, error: false });
+        }
+
+        resolve({ imgOk: true, error: false });
+      });
+
+      image.addEventListener('error', function () {
+        reject({ error: true });
+      });
+
+      image.src = url;
+    });
+  },
+};
