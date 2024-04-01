d8145cb627c9d489b8d74cffac3ff76f53e35073
HenriqueMendesCoelho <henriquemendescoelho@gmail.com>
2024-04-01 12:05:11 -0300

    style: april fools

--- src/layout/layoutWithSideBar/sidebar/sidebarRouteButtons/SidebarRouteButtons.vue
+++ src/layout/layoutWithSideBar/sidebar/sidebarRouteButtons/SidebarRouteButtons.vue
@@ -1,12 +1,12 @@
 <template>
-  <div v-for="route in routes" :key="route.text">
+  <div v-for="(route, index) in routes" :key="route.text" :class="`${index === 0 && 'q-mt-sm'}`">
     <router-link
       v-if="(userStore.isAdmin && route.isAdm) || !route.isAdm"
       :to="route.to"
       class="button"
       :class="`${route.badge && 'relative-position'}`"
     >
-      <q-badge v-if="route.badge" class="q-mr-sm" :label="route.badge" color="kb-primary" rounded floating />
+      <q-badge v-if="route.badge" class="q-mr-sm" :label="route.badge" :color="route.badgeColor || 'kb-primary'" rounded floating />
       <span class="material-icons">{{ route.icon }}</span>
       <span class="text" v-if="props.showTextsSideBar">{{ route.text }}</span>
       <CustomTooltip class="text-uppercase" anchor="top right" :offset="[35, 0]" v-if="!isSideBarExpanded" :delay="500">{{
@@ -31,6 +31,7 @@ type RouteType = {
   text: string;
   isAdm: boolean;
   badge?: string;
+  badgeColor?: string;
 }[];
 
 const props = defineProps<{
@@ -46,42 +47,56 @@ const routes: RouteType = [
     icon: 'home',
     text: 'Home',
     isAdm: false,
+    badge: 'novo',
+    badgeColor: 'kb-primary',
   },
   {
     to: { name: 'adm' },
     icon: 'admin_panel_settings',
     text: 'Painel ADM',
     isAdm: false,
+    badge: 'novo',
+    badgeColor: 'red',
   },
   {
     to: { name: 'profile' },
     icon: 'person',
     text: 'Perfil',
     isAdm: false,
+    badge: 'novo',
+    badgeColor: 'green',
   },
   {
     to: { name: 'add' },
     icon: 'add',
     text: 'Adicionar Filme',
     isAdm: false,
+    badge: 'novo',
+    badgeColor: 'pink',
   },
   {
     to: { name: 'charts' },
     icon: 'dashboard',
     text: 'Dashboard',
     isAdm: false,
+    badge: 'novo',
+    badgeColor: 'black',
   },
   {
     to: { name: 'discoverMovies' },
     icon: 'search',
     text: 'Descobrir Filmes',
     isAdm: false,
+    badge: 'novo',
+    badgeColor: 'orange',
   },
   {
     to: { name: 'wishlist' },
     icon: 'list',
     text: 'Lista de Filmes',
     isAdm: false,
+    badge: 'novo',
+    badgeColor: 'teal',
   },
 ];
 </script>
